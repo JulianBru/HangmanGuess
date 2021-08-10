99bea023cfd45a19aa187f1fc3171c5337d49a13
JulianBru <76262507+JulianBru@users.noreply.github.com>
2021-08-10 12:31:58 +0200

    Debug routine
    Initial commit

---
+++ Galgenraten 3000Tests/Business/DasSpielTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Galgenraten_3000.Business;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Galgenraten_3000.Business.Tests
+{
+    [TestClass()]
+    public class DasSpielTests
+    {
+        [TestMethod()]
+        public void DasSpielTest()
+        {
+            var spiel = new DasSpiel();
+            Assert.IsTrue(spiel.GeheimesWort.Length > 0);
+            Assert.AreEqual(spiel.GeheimesWort.Length, spiel.Anzeige.Length);
+        }
+
+        [TestMethod()]
+        public void EingabeTest()
+        {
+            var spiel = new DasSpiel();
+            spiel.GeheimesWort = "Klaus"; spiel.Anzeige = "-----";
+            spiel.Eingabe("x");
+            Assert.AreEqual(1, spiel.AnzahlFehler);
+            spiel.Eingabe("a"); spiel.Eingabe("s"); spiel.Eingabe("u"); spiel.Eingabe("l"); spiel.Eingabe("k");
+            Assert.IsTrue(spiel.IstGewonnen);
+
+        }
+
+      
+    }
+}
