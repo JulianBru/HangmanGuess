f6c2088aa92b3bb711dd2130412f25ad3d513eb9
JulianBru <76262507+JulianBru@users.noreply.github.com>
2021-08-10 12:30:50 +0200

    Initial commit
    Initial commit

---
+++ Galgenraten 3000/Business/DasSpiel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Galgenraten_3000.Business
+{
+    public class DasSpiel
+    {
+        public string GeheimesWort { get; set; }
+        public string Anzeige { get; set; }
+        public int AnzahlFehler { get; set; }
+
+        public bool IstGewonnen
+        {
+            get
+            {
+                return GeheimesWort.Equals(Anzeige);
+            }
+        }
+        public bool IstTot
+        {
+            get
+            {
+                return AnzahlFehler > 10;
+            }
+        }
+
+        public DasSpiel()  //Default-Konstruktor
+        {
+            var alle = File.ReadAllLines(@"c:\temp\maco\deutsch.txt");
+            var r = new Random();
+            GeheimesWort = alle[r.Next(0,alle.Length)];
+            Anzeige = new string('-', GeheimesWort.Length);
+            AnzahlFehler = 0;
+        }
+
+        public void Eingabe(string zeichen)
+        {
+            var tmp = "";
+            var gefunden = false;
+            for (int i = 0; i < GeheimesWort.Length; i++)
+            {
+                if (GeheimesWort.ToUpper()[i]==zeichen.ToUpper()[0])
+                {
+                    tmp += GeheimesWort[i];
+                    gefunden = true;
+                }
+                else
+                {
+                    tmp += Anzeige[i];
+                }
+            }
+            Anzeige = tmp;
+            if (!gefunden)
+            {
+                AnzahlFehler++;
+            }
+
+        }
+
+    }
+}
---
+++ Galgenraten 3000/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Galgenraten_3000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Willkommen zum Galgenraten...");
+            var spiel = new Business.DasSpiel();
+            while (true)
+            {
+                Console.WriteLine(spiel.Anzeige);
+                Console.Write($"{spiel.AnzahlFehler} Fehler> ");
+
+                spiel.Eingabe(Console.ReadLine());
+                if (spiel.IstGewonnen || spiel.IstTot)
+                {
+                    break;
+                }
+            } //ende von while
+            if (spiel.IstGewonnen)
+            {
+                Console.WriteLine("Herzlichen Glückwunsch, Alter...");
+            }
+            else
+            {
+                Console.WriteLine($"Pech gehabt, Alter... ({spiel.GeheimesWort})");
+            }
+
+        }
+    }
+}
